94350a429716f878fc436a9a7eea029d31d99b1e
Max Ohsawa <max.ohsawa@gmail.com>
2024-02-26 20:43:12 -0500

    adds logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+import "./App.css";
+import logo from "./assets/mustang-logo-color.jpg";
+
 function App() {
   return (
     <>
       <h1>Mustang Club of Charlotte County</h1>
       <h2>Coming soon...</h2>
+      <img src={logo} />
     </>
   );
 }
